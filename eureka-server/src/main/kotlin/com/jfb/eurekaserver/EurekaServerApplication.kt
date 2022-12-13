dfa1cd78e2f8f278a2c32a9952dafde5e1cff01d
Josivaldo Batista <josivaldo-fb@outlook.com>
2022-12-13 08:35:07 -0300

    first commit

---
+++ eureka-server/src/main/kotlin/com/jfb/eurekaserver/EurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.jfb.eurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurekaServerApplication>(*args)
+}
