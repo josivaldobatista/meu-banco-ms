b5f911d01767b8d44972de35924781f501f2cfed
Josivaldo Batista <josivaldo-fb@outlook.com>
2022-12-16 00:27:38 -0300

    Configuring microservice cliente

---
+++ ms-clientes/src/test/kotlin/com/jfb/msclientes/MsClientesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jfb.msclientes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
