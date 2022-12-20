9e37223b1f80ab74da9e8908dfaa959cd2865d6b
Josivaldo Batista <josivaldo-fb@outlook.com>
2022-12-20 11:07:14 -0300

    Criado Microservice de cartões

---
+++ ms-cartoes/src/main/kotlin/com/jfb/mscartoes/MsCartoesApplication.kt
@@ -0,0 +1,11 @@
+package com.jfb.mscartoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsCartoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsCartoesApplication>(*args)
+}
---
+++ ms-cartoes/src/test/kotlin/com/jfb/mscartoes/MsCartoesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jfb.mscartoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsCartoesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
