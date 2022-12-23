47a15223c338cbf76ff19da43857fc11334d93b0
Josivaldo Batista <josivaldo-fb@outlook.com>
2022-12-22 22:16:46 -0300

    Feature: Criação do Microsserviço de avaliar crédito

---
+++ ms-avaliador-credito/src/main/kotlin/com/jfb/msavaliadorcredito/MsAvaliadorCreditoApplication.kt
@@ -0,0 +1,11 @@
+package com.jfb.msavaliadorcredito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsAvaliadorCreditoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsAvaliadorCreditoApplication>(*args)
+}
